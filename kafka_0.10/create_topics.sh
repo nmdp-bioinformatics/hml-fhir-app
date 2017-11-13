e4e7b9cf35224b5a41ff7f434a25969e432dc62d
Andrew S. Brown, Ph.D <abrown3@nmdp.org>
2017-11-12 20:50:29 -0600

    Update Kafka-create-topics

--- kafka_0.10/create_topics.sh
+++ kafka_0.10/create_topics.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-cd kafka_2.12-0.10.2.1
+cd /usr
 
 sh bin/kafka-topics.sh --create --zookeeper kafka:2181 --replication-factor 1 --partitions 1 --topic fhir-submission
 sh bin/kafka-topics.sh --create --zookeeper kafka:2181 --replication-factor 1 --partitions 1 --topic hml-fhir-conversion
